a32052b479b724f0112a0cebec8f963e4871c831
Helari <bastien.nanceau@gmail.com>
2015-01-29 17:03:25 +0100

    Corridor Class

---
+++ Hallucination/Assets/Scripts/Corridor.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Corridor : MonoBehaviour {
+
+    public int m_nbExit;
+    public Transform[] m_exitPositions;
+    private Corridor[] m_children = null;
+
+	// Use this for initialization
+	void Start () {
+        m_children = new Corridor[m_nbExit];
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter(Collider _other)
+    {
+        if (_other.CompareTag("Player") && _other.GetComponent<Player>().GetCurrentLocation() != this.transform.GetInstanceID())
+        {
+            InstantiateChildren();
+            for (int i = 0; i < m_nbExit; i++)
+            {
+                m_children[i].PropagateDeallocation(this);
+            }
+            _other.GetComponent<Player>().SetCurrentLocation(this.transform.GetInstanceID());
+        }
+    }
+
+    Corridor InstantiateCorridor(Transform exitPosition)
+    {
+        Corridor tempCorridor;
+        int randomCorridorID = (int) Random.Range(0,4);
+        tempCorridor = Instantiate(Resources.Load("Corridor#"+randomCorridorID.ToString())) as Corridor;
+        tempCorridor.transform.position = new Vector3(exitPosition.position.x, exitPosition.position.y, exitPosition.position.z);
+
+        int randomExitID = (int)Random.Range(0, tempCorridor.m_nbExit);
+        tempCorridor.transform.localEulerAngles = new Vector3(tempCorridor.m_exitPositions[randomExitID].localEulerAngles.x + exitPosition.localEulerAngles.x, tempCorridor.m_exitPositions[randomExitID].localEulerAngles.y + exitPosition.localEulerAngles.y, tempCorridor.m_exitPositions[randomExitID].localEulerAngles.z + exitPosition.localEulerAngles.z);
+        
+        /***** Rescale -1 to revert 'L-shaped' Corridor -> problem when not using the "default" exitPoint
+        if(randomCorridorID == 1) {
+            if ((int)Random.Range(0, 100) % 2 == 0)
+            {
+                tempCorridor.transform.localScale = new Vector3(1f, -1f, 1f);
+            }
+        }
+        */
+
+        return tempCorridor;
+    }
+
+    void InstantiateChildren()
+    {
+        for (int i = 0; i < m_nbExit; i++)
+        {
+            if (m_children[i] == null)
+            {
+                Corridor RandomCorridor = new Corridor(); //Instantiate(RandomCorridor);
+                //m_children[i] = CreateInstance(m_exitPositions[i]);
+                RandomCorridor.AddExit(this);
+                if (RandomCorridor.m_nbExit > 2)
+                {
+                    RandomCorridor.InstantiateChildren();
+                }
+            }
+        }
+    }
+    void PropagateDeallocation(Corridor parent)
+    {
+        if (m_nbExit > 2)
+        {
+            for (int i = 0; i < m_nbExit; i++)
+            {
+                if (m_children[i] != parent && m_children[i] != null)
+                {
+                    m_children[i].PropagateDeallocation(this);
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < m_nbExit; i++)
+            {
+                Destroy(m_children[i]);
+            }
+        }
+    }
+
+    void AddExit(Corridor corridor)
+    {
+        bool result = false;
+
+        for (int i = 0; i < m_nbExit; i++)
+        {
+            if (m_children[i] != null)
+            {
+                m_children[i] = corridor;
+                result = true;
+                break;
+            }
+        }
+
+        if (!result)
+        {
+            Debug.Log("Error");
+        }
+    }
+}
