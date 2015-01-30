fc49c1813b0abd765842c978a8bc6985904f3327
Helari <bastien.nanceau@gmail.com>
2015-01-30 10:55:33 +0100

    Corridor & Player

---
+++ Hallucination/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    private int m_currentCorridor = 0;
+
+    public int GetCurrentLocation()
+    {
+        if (m_currentCorridor == 0)
+        {
+            Debug.Log("There might be an error, current Corridor is uninitialized");
+        }
+
+        return m_currentCorridor;
+    }
+
+    public void SetCurrentLocation(int transformID)
+    {
+        m_currentCorridor = transformID;
+    }
+}
